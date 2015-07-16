ee131a9d7290e39f1e3837e086fb78e8828e0b32
Danny Navarro <j@dannynavarro.net>
2015-07-16 15:03:49 +0200

    Freeze for lts-7.8, ghc-7.10 and debian jessie dependencies

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE CPP #-}
 module Main where
 
 import System.FilePath.Glob (glob)
 import Test.DocTest (doctest)
 
 main :: IO ()
+#if !MIN_VERSION_doctest(0,10,1)
+main = doctest . ([ "-idist/build/autogen"
+                  , "-optP-include"
+                  , "-optPdist/build/autogen/cabal_macros.h"
+                  ] ++) =<< glob "Test/**/*.hs"
+#else
 main = doctest =<< glob "Test/**/*.hs"
+#endif
