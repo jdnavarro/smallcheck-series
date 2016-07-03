135a1c85d2c439d47c2f5f0f640bdbe4f5080c9b
Danny Navarro <j@dannynavarro.net>
2016-07-03 12:37:27 -0400

    Drop support for Debian dependencies

--- scripts/deb-deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-grep-aptavail -F Ghc-Package . -s Ghc-Package -n \
-    | sort \
-    | perl -ne 'BEGIN {print "constraints:\n"} /^(.*)-([0-9\.]*)-(.*)$/; print "  $1 ==$2,\n"' \
-    | sed '$s/,$//' \
-    > cabal-deb.config
